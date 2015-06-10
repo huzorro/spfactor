2f543ab3173c8322f1b2dbd1532f7bb0596ee778
huzorro <huzorro@gmail.com>
2015-06-10 16:36:51 +0800

    移除通过队列数量控制进程运行的方法

--- sexredis/queue.go
+++ sexredis/queue.go
@@ -21,9 +21,9 @@ import (
 
 //	})
 type Queue struct {
-	name    string
-	rc      *redis.Client
-	msgchan chan Msg
+	Name    string
+	Rc      *redis.Client
+	Msgchan chan Msg
 }
 
 type Msg struct {
@@ -41,39 +41,39 @@ func New() *Queue {
 }
 
 func (self *Queue) SetRClient(name string, rc *redis.Client) {
-	self.name = name
-	self.rc = rc
+	self.Name = name
+	self.Rc = rc
 }
 
 func (self *Queue) Create(name string, host string, port uint) error {
-	self.name = name
-	self.rc = redis.New()
-	return self.rc.Connect(host, port)
+	self.Name = name
+	self.Rc = redis.New()
+	return self.Rc.Connect(host, port)
 }
 
 func (self *Queue) Active() (response string, err error) {
-	return self.rc.Ping()
+	return self.Rc.Ping()
 }
 
 func (self *Queue) LLen() (l int64, err error) {
-	return self.rc.LLen(self.name)
+	return self.Rc.LLen(self.Name)
 }
 
 func (self *Queue) Keys() (name string) {
-	return self.name
+	return self.Name
 }
 
 func (self *Queue) Clear() (recode int64, err error) {
-	return self.rc.Del(self.name)
+	return self.Rc.Del(self.Name)
 }
 
 func (self *Queue) Close() (string, error) {
-	return self.rc.Quit()
+	return self.Rc.Quit()
 }
 
 func (self *Queue) Get(block bool, timeout uint64) (msg Msg) {
 	if block {
-		if rs, err := self.rc.BLPop(timeout, self.name); err != nil {
+		if rs, err := self.Rc.BLPop(timeout, self.Name); err != nil {
 			return Msg{"", err}
 		} else {
 			if len(rs) > 1 {
@@ -83,20 +83,20 @@ func (self *Queue) Get(block bool, timeout uint64) (msg Msg) {
 			}
 		}
 	} else {
-		m, err := self.rc.LPop(self.name)
+		m, err := self.Rc.LPop(self.Name)
 		return Msg{m, err}
 	}
 }
 
 func (self *Queue) Put(msg interface{}) (recode int64, err error) {
-	return self.rc.RPush(self.name, msg)
+	return self.Rc.RPush(self.Name, msg)
 }
 
 /*
 use channel implement like python yield
 */
-func (self *Queue) consume() {
-	self.msgchan = make(chan Msg)
+func (self *Queue) Consume() {
+	self.Msgchan = make(chan Msg)
 	for {
 		//		if msg, err := self.Get(true, uint64(0)); err != nil {
 		//			return err
@@ -104,12 +104,12 @@ func (self *Queue) consume() {
 		//			self.msgchan <- msg
 		//		}
 
-		self.msgchan <- self.Get(true, uint64(0))
+		self.Msgchan <- self.Get(true, uint64(0))
 	}
 }
 
-func (self *Queue) yield() (msg Msg) {
-	return <-self.msgchan
+func (self *Queue) Yield() (msg Msg) {
+	return <-self.Msgchan
 }
 
 //func (self *Queue) Worker(pnum uint, fn func(msg string, err error)) {
@@ -138,43 +138,12 @@ func (self *Queue) yield() (msg Msg) {
 func (self *Queue) Worker(pnum uint, serial bool, ps ...Processor) {
 	control := make(chan Msg, pnum)
 	go func() {
-		self.consume()
+		self.Consume()
 	}()
 
 	go func() {
 		for {
-			msg := self.yield()
-			control <- msg
-			go func() {
-				if serial {
-					for _, sp := range ps {
-						sp.SProcess(&msg)
-						if msg.Err != nil {
-							break
-						}
-					}
-				} else {
-					//并行处理
-				}
-				<-control
-			}()
-		}
-	}()
-}
-
-func (self *Queue) Control(pnum uint, serial bool, ps ...Processor) {
-	control := make(chan Msg, pnum)
-	lenChan := make(chan Msg)
-	go func() {
-		for {
-			if n, _ := self.LLen(); n > 0 {
-				lenChan <- Msg{n, nil}
-			}
-		}
-	}()
-	go func() {
-		for {
-			msg := <-lenChan
+			msg := self.Yield()
 			control <- msg
 			go func() {
 				if serial {
